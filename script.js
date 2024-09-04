a1b73e4a12f7b2ea5efe215fdcec328122ca234d
TIWIMIN <tiwimin@gmail.com>
2024-09-04 14:17:22 -0700

    Implement game controller as IIFE

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function GameBoard() {
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
-        for (let j = 0; i < columns; j++) {
+        for (let j = 0; j < columns; j++) {
             board[i].push(Cell());
         }
     }
@@ -17,7 +17,7 @@ function GameBoard() {
         if (board[markedRow][markedColumn].getValue() !== 0) {
             return;
         }
-        board[row][column].addToken(player);
+        board[markedRow][markedColumn].addToken(player);
     };
     const printBoard = () => {
         for (let i = 0; i < rows; i++) {
@@ -45,9 +45,49 @@ function Cell() {
     };
 }
 
+
 const game = (function GameController(
     playerOneName = "Player One", 
     playerTwoName = "Player Two"
 ) {
+    const board = GameBoard(); 
 
+    const players = [
+        {
+            name: playerOneName,
+            token: 'X'
+        }, 
+        {
+            name: playerTwoName,
+            token: 'O'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer; 
+
+    const printNewRound = () => {
+        board.printBoard(); 
+        console.log(`${getActivePlayer().name}'s turn.`);
+
+    }
+
+    const playRound = (row, column) => {
+        board.markCell(row, column, getActivePlayer().token);
+        switchPlayerTurn(); 
+        printNewRound();
+    }
+
+    printNewRound(); 
+
+    return {
+        playRound, 
+        getActivePlayer, 
+        getBoard: board.getBoard
+    };
 })();
